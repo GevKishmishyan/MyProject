5e3ca879b154ea85e4047662041b6c9150808098
Arthur <arturakhachatryan1991@gmail.com>
2022-03-17 00:53:00 +0400

    "first commit in my branch"

--- src/test/java/com/example/myproject/MyControllerTest.java
+++ src/test/java/com/example/myproject/MyControllerTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-////
+
 class MyControllerTest {
-    //////////
+
     @BeforeEach
     void setUp() {
     }
